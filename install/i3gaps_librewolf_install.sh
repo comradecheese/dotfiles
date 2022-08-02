9f7e034c955f1eac3b877536c4746c520a3604bb
Comrade Cheese <jervy_77@hotmail.com>
2022-08-02 15:49:36 +1000

    added dotfiles setup script

--- install/i3gaps_librewolf_install.sh
+++ install/i3gaps_librewolf_install.sh
@@ -3,7 +3,7 @@ echo "Updating system..."
 sudo apt update -y && sudo apt upgrade -y
 
 echo "Installing i3-gaps dependencies..."
-sudo apt install meson dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev xorg xinit xsettingsd build-essential suckless-tools i3status i3lock fonts-mplus rxvt-unicode xsel lxappearance arc-theme -y
+sudo apt install meson dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev xorg xinit xsettingsd build-essential suckless-tools i3status i3lock fonts-mplus rxvt-unicode xsel lxappearance arc-theme compton -y
 
 echo "Cloning i3-gaps repo..."
 git clone https://github.com/Airblader/i3 i3-gaps
@@ -25,16 +25,4 @@ sudo cat librewolfsource >> /etc/apt/sources.list.d/librewolf.sources
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install librewolf -y
 
-echo "Setting up dotfiles..."
-cd ..
-cp -rv .config/ ~
-cp -v .Xauthority ~
-cp -v .Xresources ~
-cp -v .bash_profile ~
-cp -v .wallpaper.jpg ~
-cp -v .xinitrc ~
-cp -v .xsettingsd ~
-
 echo "Finished!"
-read -p "Press any key to reboot... " -n1 -s
-sudo reboot now
---
+++ install/setup_dotfiles.sh
@@ -0,0 +1,12 @@
+echo "Ricing!"
+
+cd ..
+cp -rv .config/ ~
+cp -v .bash_profile ~
+cp -v .wallpaper.jpg ~
+cp -v .Xauthority ~
+cp -v .xinitrc ~
+cp -v .Xresources ~
+cp -v .xsettingsd ~
+
+echo "Finished!"

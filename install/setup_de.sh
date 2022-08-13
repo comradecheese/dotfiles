52b645cd76ae496a517363e80267e3c935cd4118
Comrade Cheese <jervy_77@hotmail.com>
2022-08-13 13:20:16 +1000

    added ufw and chkrootkit

--- install/setup_de.sh
+++ install/setup_de.sh
@@ -13,7 +13,7 @@ banner "Desktop Environment Setup!"
 sudo apt update -y && sudo apt upgrade -y
 
 banner "Installing required packages"
-sudo apt install meson dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev xorg xinit xsettingsd build-essential suckless-tools i3status i3lock-fancy fonts-mplus rxvt-unicode xsel lxappearance arc-theme compton feh neofetch scrot figlet -y
+sudo apt install meson dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev xorg xinit xsettingsd build-essential suckless-tools i3status i3lock-fancy fonts-mplus rxvt-unicode xsel lxappearance arc-theme compton feh neofetch scrot figlet htop chkrootkit ufw -y
 
 banner "Installing i3-gaps"
 git clone https://github.com/Airblader/i3 i3-gaps
@@ -32,5 +32,10 @@ wget -O- https://deb.librewolf.net/keyring.gpg | sudo gpg --dearmor -o /usr/shar
 sudo echo -e "Types: deb\nURIs: https://deb.librewolf.net\nSuites: $distro\nComponents: main\nArchitectures: amd64\nSigned-By: /usr/share/keyrings/librewolf.gpg" > /etc/apt/sources.list.d/librewolf.sources
 sudo apt update -y
 sudo apt install librewolf -y
+
+banner "Setup and enable firewall"
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw enable
 
 banner "Finished!"
